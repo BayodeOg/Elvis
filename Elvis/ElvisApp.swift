617f710f95230099656581f17f18f066c3489c7d
bayo <bayodeog@gmail.com>
2026-01-21 17:38:59 +0100

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Adebayode Adegoke on 21/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
